980156379ab0f1a717c2e9f9345cfa7f8d4cfe04
Glorypixel <ifeomaglory1998@gmail.com>
2024-06-08 17:22:23 +0100

    added a new change

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const searchBox = document.getElementById("search-item");
-const productList = document.getElementById("product-list");
-const product = document.querySelectorAll(".products");
-const pname = document.getElementsByTagName("h2")
 const search = () => {
-  searchBox.value.toUpperCase();
- for 
+  const searchBox = document.getElementById("search-item").value.toUpperCase();
+  const productList = document.getElementById("product-list");
+  const products = document.querySelectorAll(".products");
+  const pname = productList.getElementsByTagName("h2");
+
+  for (let i = 0; i < pname.length; i++) {
+    let match = products[i].getElementsByTagName("h2")[0];
+    if (match) {
+      let textvalue = match.textContent || match.innerHTML;
+      if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
+        products[i].style.display = "";
+      } else {
+        products[i].style.display = "none";
+      }
+    } 
+  }
 };
+
