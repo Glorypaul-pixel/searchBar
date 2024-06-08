9d879eb29a84e2d59bdb251908f22f85cba5a19e
Glorypixel <ifeomaglory1998@gmail.com>
2024-06-08 11:53:03 +0100

    added js

---
+++ script.js
@@ -0,0 +1,8 @@
+const searchBox = document.getElementById("search-item");
+const productList = document.getElementById("product-list");
+const product = document.querySelectorAll(".products");
+const pname = document.getElementsByTagName("h2")
+const search = () => {
+  searchBox.value.toUpperCase();
+ for 
+};
